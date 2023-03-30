dcacfd3c7ff524f2037d601454f16890abf4d63f
Josh <hello@josh.ee>
2023-03-30 16:01:11 +0100

    Determine API base from NODE_ENV

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,6 @@
 import fetch from 'node-fetch'
 
-const apiBase = 'http://localhost'
+const apiBase = process.env.NODE_ENV === 'development' ? 'http://localhost' : 'https://api.mono.domains'
 
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
