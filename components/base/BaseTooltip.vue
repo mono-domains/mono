4d3880d99320b31f0716d8eef18460bcacdcfc6b
Josh <hello@josh.ee>
2025-04-13 19:53:26 +0100

    Make label span full width in BaseTooltip

--- components/base/BaseTooltip.vue
+++ components/base/BaseTooltip.vue
@@ -1,13 +1,16 @@
 <template>
   <span class="group inline-block relative" tabindex="0">
-    <span :aria-describedby="ariaId"><slot name="label" /></span>
+    <span :aria-describedby="ariaId" class="w-full"><slot name="label" /></span>
 
     <span
       role="tooltip"
       :id="ariaId"
-      class="hidden sm:block absolute -top-1.5 left-1/2 -translate-x-1/2 -translate-y-full w-max max-w-xs text-xs font-semibold text-center bg-black text-white px-2 py-1 rounded opacity-0 group-hover:opacity-100 group-focus:opacity-100 transition-opacity duration-300 pointer-events-none">
+      class="hidden sm:block absolute -top-1.5 left-1/2 -translate-x-1/2 -translate-y-full w-max max-w-xs text-xs font-semibold text-center bg-black text-white px-2 py-1 rounded opacity-0 group-hover:opacity-100 group-focus:opacity-100 transition-opacity duration-300 pointer-events-none"
+    >
       <slot name="tooltip" />
-      <span class="absolute bottom-px left-1/2 -translate-x-1/2 translate-y-full border-black border-4 border-l-transparent border-r-transparent border-b-transparent" />
+      <span
+        class="absolute bottom-px left-1/2 -translate-x-1/2 translate-y-full border-black border-4 border-l-transparent border-r-transparent border-b-transparent"
+      />
     </span>
   </span>
 </template>
@@ -18,8 +21,8 @@ export default {
   props: {
     ariaId: {
       type: String,
-      default: ''
-    }
-  }
+      default: '',
+    },
+  },
 }
 </script>
